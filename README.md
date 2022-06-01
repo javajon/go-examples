8fa7e74dfecc4343dd7ea8c2f1d75afe8a6d9489
javajon <jonathan.johnson@dijure.com>
2022-06-01 00:11:55 -0400

    Fixed weather app to use new server. Upgraded go code

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 Here are some examples of running [Go](https://golang.org/) applications in containers and on Kubernetes. The examples range from a simple Hello World application to basic microservice with [CRUD](https://en.wikipedia.org/wiki/Create,_read,_update_and_delete) actions.
 
 This source code is referenced by the Katacoda scenario [Go Apps to Kubernetes](https://katacoda.com/javajon/courses/kubernetes-containers).
+
+## Weather Apps
+
+The `basic` folder demonstrates a small Go app that obtains the current weather in London. A The Open-Meteo project offers a Weather Forecast API and provides a [REST URL builder](https://open-meteo.com/en/docs).
+
+Given a request like this:
+
+`curl https://api.open-meteo.com/v1/forecast?latitude=51.5002&longitude=-0.1262&daily=weathercode&timezone=Europe%2FLondon`
+
+The result would be:
+
+```json
+{
+  "longitude": -0.120000124,
+  "daily_units": {
+    "time": "iso8601",
+    "weathercode": "wmo code"
+  },
+  "latitude": 51.5,
+  "generationtime_ms": 0.29206275939941406,
+  "elevation": 13.1953125,
+  "daily": {
+    "time": [
+      "2022-06-01",
+      "2022-06-02",
+      "2022-06-03",
+      "2022-06-04",
+      "2022-06-05",
+      "2022-06-06",
+      "2022-06-07"
+    ],
+    "weathercode": [
+      80,
+      45,
+      3,
+      3,
+      80,
+      63,
+      61
+    ]
+  },
+  "utc_offset_seconds": 3600
+}
+```
+
+The demonstration app simply makes this call to discover the somewhat predictable drizzly conditions in London.
--- basic/main.go
+++ basic/main.go
@@ -3,14 +3,48 @@ package main
 import (
     "fmt"
     "io/ioutil"
-	"net/http"
-	"strings"
-	"os"
+    "net/http"
+    "strings"
+    "os"
 
-	"github.com/tidwall/gjson"
+    "github.com/tidwall/gjson"
 )
 
-const london string = "44418"
+const city string = "London"
+
+const locationLat string = "51.5002"
+const locationLon string = "-0.1262"
+
+var codes = map[string]string{
+    "0": "Clear sky",
+    "1": "Mainly clear",
+    "2": "Partly cloudy",
+    "3": "Overcast",
+    "45": "Fog and depositing rime fog",
+    "48": "Depositing rime fog",
+    "51": "Drizzle: Light intensity",
+    "53": "Drizzle: Moderate intensity",
+    "55": "Drizzle: Dense intensity",
+    "56": "Freezing Drizzle: Light intensity",
+    "57": "Freezing Drizzle: Dense intensity",
+    "61": "Rain: Slight intensity",
+    "63": "Rain: Moderate intensity",
+    "65": "Rain: Heavy intensity",
+    "66": "Freezing Rain: Light intensity",
+    "67": "Freezing Rain: Heavy intensity",
+    "71": "Snow fall: Slight intensity",
+    "73": "Snow fall: Moderate intensity",
+    "75": "Snow fall: Heavy intensity",
+    "77": "Snow grains",
+    "80": "Rain showers: Slight",
+    "81": "Rain showers: Moderate",
+    "82": "Rain showers: Violent",
+    "85": "Snow showers: Slight",
+    "86": "Snow showers: Heavy",
+    "95": "Thunderstorm: Slight or moderate",
+    "96": "Thunderstorm with slight hail",
+    "99": "Thunderstorm with heavy hail",
+}
 
 func check(err error) {
     if err != nil {
@@ -19,17 +53,23 @@ func check(err error) {
     }
 }
 
-func weatherOrNot(location string) {
-    resp, err := http.Get("https://www.metaweather.com/api/location/" + location)
+func weatherOrNot(city string, latitude string, longitude string) {
+    request := []string{
+                "https://api.open-meteo.com/v1/forecast?",
+                "latitude=", latitude, 
+                "&longitude=", longitude, 
+                "&daily=weathercode&timezone=Europe%2FLondon"}
+
+    resp, err := http.Get(strings.Join(request[:], ""))
     check(err)
-    body, err := ioutil.ReadAll(resp.Body)
-	check(err)
-	
-	title := gjson.Get(string(body), "title")
-	value := gjson.Get(string(body), "consolidated_weather.0.weather_state_name")
-	fmt.Printf("The current weather condition in %s is %s.\n", title, strings.ToLower(value.String()))
+    json, err := ioutil.ReadAll(resp.Body)
+    check(err)
+    
+    currentWeatherCode := gjson.Get(string(json), "daily.weathercode.0")
+    value := codes[currentWeatherCode.String()]
+    fmt.Printf("The current weather condition in %s is %s.\n", city, value)
 }
 
 func main() {
-	weatherOrNot(london)
+    weatherOrNot(city, locationLat, locationLon)
 }
